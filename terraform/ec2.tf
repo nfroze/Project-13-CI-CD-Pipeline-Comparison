db99aa18f5c887b6e2a0fdeee65998cfd19f34d6
Noah <Noahfrost@outlook.com>
2025-08-31 21:41:09 +0100

    Update pipeline configurations for actual deployment stages
    - Add test and deployment stages to Jenkinsfile
    - Move .gitlab-ci.yml to root directory for GitLab
    - Include all environment stages (dev, staging, prod)

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "gitlab" {
   vpc_security_group_ids = [aws_security_group.cicd_tools.id]
 
     root_block_device {
-    volume_size = 20
+    volume_size = 30
     }
 
   tags = {
